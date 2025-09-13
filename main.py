08177da0cad16915cca48073d8f36246b47e3c17
Salsabeel <salbeel.jfr@gmail.com>
2025-09-13 21:17:43 +0300

    added schema and routes

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+#PYDANTIC MODELS 
+#pydanyic modles so we tell the frontend devs what to expect
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+#POST ROOT
+@app.post("/posts")
+def posts(post: Post): #DATA VALIDATION
+    return{"data":post}
+
+
 
 @app.get("/")
 def root():
     return {"message": "mew!"}
+
