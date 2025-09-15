3826bf247a3cc1b905009df1f63b69c586892abc
Salsabeel <salbeel.jfr@gmail.com>
2025-09-15 19:25:42 +0300

    gitting started with databaces

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+import schemas
+from typing import List
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 
 app = FastAPI()
 
-#PYDANTIC MODELS 
-#pydanyic modles so we tell the frontend devs what to expect
 
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-#POST ROOT
-@app.post("/posts")
-def posts(post: Post): #DATA VALIDATION
-    return{"data":post}
-
-
+#connecting to the database
+conn = psycopg2.connect(
+    host="localhost",
+    database="fastapi",
+    user="postgres",
+    password="7364",
+    cursor_factory=RealDictCursor
+)
+cur=conn.cursor()
 
 @app.get("/")
 def root():
     return {"message": "mew!"}
+
+#GET ALL POSTS
+@app.get("/posts")#from api to user
+def get_posts(): #DATA VALIDATION, from user to api
+    cur.execute("select * from posts")
+    posts=cur.fetchall()
+    return posts
+
+#CREATE ONE POST
+@app.post("/posts")
+def create_post(post: dict):# turning json into python dictionarry
+    cur.execute("insert into posts (title, content, published) values (%s, %s, %s) returning *",
+                (post['title'], post['content'], post['published', True]))
+    new_post=cur.fetchone()
+    conn.commit()
+    return new_post
+
+
+
 
---
+++ schemas.py
@@ -0,0 +1,34 @@
+# to sepetare the schema modles from the rest of the code
+#PYDANTIC MODELS 
+#pydanyic modles so we tell the frontend devs what to expect
+from pydantic import BaseModel
+import datetime
+
+
+
+class PostBase(BaseModel):# common between req and res
+    title: str
+    content: str
+    published: bool = True
+
+
+
+
+
+
+
+
+
+class PostCreate(PostBase):#req input schema, if i wanted the user to send me some data but i dont want it to be returned to them
+    pass 
+
+
+
+# class Post(PostBase):#res, fields r from the database, output schema
+#     id: int
+#     created_at: datetime
+#     owner_id: int 
+
+#     class Config:#transforming obj from sqlalchemy to json
+#         orm_mode= True 
+
