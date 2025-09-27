37e931678f1f5e6061f7d6fbf932293909d03bb2
Salsabeel <salbeel.jfr@gmail.com>
2025-09-27 23:12:32 +0300

    Added http exceptions aling with some data validation :7

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 import schemas
 import psycopg2
 from psycopg2.extras import RealDictCursor
@@ -39,8 +39,11 @@ def get_posts(): #DATA VALIDATION, from user to api
     posts=cur.fetchall()
     return posts
 
+
+
+
 #CREATE ONE POST
-@app.post("/posts", response_model=schemas.Post)
+@app.post("/posts", response_model=schemas.Post, status_code=status.HTTP_201_CREATED)
 def create_post(post: schemas.PostCreate):# data validation
     cur.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,(post.title, post.content, post.published))
     new_post=cur.fetchone()
@@ -51,40 +54,39 @@ def create_post(post: schemas.PostCreate):# data validation
 
 #GETTING ONE POST
 @app.get("/posts/{id}", response_model=schemas.Post)
-def get_one_post(id):
+def get_one_post(id: int):
     cur.execute("""SELECT * FROM posts WHERE id = %s""", (id,))
     the_post=cur.fetchone()
 
     if the_post is None:
-        return {"Error": f"Post with id {id} was not found"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} wasnt found")
+    
     return the_post
 
 
 #DELETING A POST
-@app.delete("/posts/{id}")
-def delete_post(id):
-
-    
-
-    cur.execute("""DELETE FROM posts WHERE id = %s RETURNING""", (id,))
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    cur.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (id,))
     deleted_post = cur.fetchone()
     conn.commit()
 
     
     if deleted_post is None:
-        return {"Error": f"Post with id {id} was not found"}
-    return "DELETED"
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} wasnt found")
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 #UPDATING A POST
-@app.put("/posts/{id}")
-def update_post(id, post: schemas.PostCreate):
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, post: schemas.PostCreate):
     cur.execute("""UPDATE posts SET title = %s , content = %s , published = %s WHERE id = %s RETURNING * """, (post.title , post.content, post.published, id) )
     updated_post=cur.fetchone()
     conn.commit()
 
     if updated_post is None:
-        return {"Error": f"Post with id {id} was not found"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} wasnt found")
     return updated_post
 
 
