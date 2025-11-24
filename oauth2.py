8a1601c2cc015ad04d37949b2f8d85270d9ad154
Salsabeel <salbeel.jfr@gmail.com>
2025-11-24 20:13:55 +0300

    added jwt logic and into routs

--- oauth2.py
+++ oauth2.py
@@ -2,18 +2,24 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 import schemas
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_schema=OAuth2PasswordBearer(tokenUrl='login')#Defines how Fastapi exctracts the token, when someone logs in, they get tokens from /login
 
 SECRET_KEY="solasala558" #signing the jwt
-ALGORITHM="HS256"#how the token is generated
+ALGORITHM="HS256"#how the token is generated, encryption algorithm
 ACCESS_TOKEN_EXPIRE_MINUTES=30 #token is valid 4 30 mins
 
 
-#token to access protected routs
+#token from client to api
 def create_access_token(data: dict):
-    to_encode=data.copy()
+    to_encode=data.copy()# data
 
     expire = datetime.utcnow()+timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
+    to_encode.update({"exp": expire}) #jwt contained exp field if i want it to expire automatically 
+
+    #now the payload has a data(user_id) and exp
 
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM )
 
@@ -26,8 +32,21 @@ def verify_access_token(token: str, credintials_exception):
 
         if id in None:
             raise credintials_exception
-        return schemas.TokenData(id=id)
+        return schemas.TokenData(id=id)#warp id into a pydamtic model
     
     
     except JWTError:
         raise credintials_exception
+    
+
+def get_current_user(token: str = Depends(oauth2_schema)):
+    credintials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate:": "Bearer"}
+    )
+
+    return verify_access_token(token, credintials_exception)#validate token & returns tokendata to the route handler
+    
+
+
--- routers/posts.py
+++ routers/posts.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 import schemas, models, database
 from typing import List
 from database import get_db
+import oauth2
 
 router = APIRouter(
     prefix="/posts", #so we can replace /posts with just /
@@ -14,7 +15,7 @@ router = APIRouter(
 
 #GET ALL POSTS
 @router.get("/", response_model=List[schemas.Post])#from api to user
-def get_posts(db: Session=Depends(get_db)): #DATA VALIDATION, from user to api
+def get_posts(db: Session=Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)): #DATA VALIDATION, from user to api, if the client provides a valid jwt, the route executes, otherwise a 401 error
     posts=db.query(models.Post).all()
     return posts
 
@@ -23,7 +24,7 @@ def get_posts(db: Session=Depends(get_db)): #DATA VALIDATION, from user to api
 
 #CREATE ONE POST
 @router.post("/", response_model=schemas.Post, status_code=status.HTTP_201_CREATED)
-def create_post(post: schemas.PostCreate, db: Session=Depends(get_db)):# data validation
+def create_post(post: schemas.PostCreate, db: Session=Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):# data validation
 
     new_post=models.Post(**post.model_dump())#convert s sqlalchemy model into dictionary
     
@@ -36,7 +37,7 @@ def create_post(post: schemas.PostCreate, db: Session=Depends(get_db)):# data va
 
 #GETTING ONE POST
 @router.get("/{id}", response_model=schemas.Post)
-def get_one_post(id: int, db: Session=Depends(get_db)):
+def get_one_post(id: int, db: Session=Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
     post=db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
@@ -47,12 +48,14 @@ def get_one_post(id: int, db: Session=Depends(get_db)):
 
 #DELETING A POST
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session=Depends(get_db)):
+def delete_post(id: int, db: Session=Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
     deleted_post = db.query(models.Post).filter(models.Post.id == id)
 
     
     if deleted_post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} wasnt found")
+    
+    
     
     deleted_post.delete(synchronize_session=False)
     db.commit()
@@ -62,7 +65,7 @@ def delete_post(id: int, db: Session=Depends(get_db)):
 
 #UPDATING A POST
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, post: schemas.PostCreate, db: Session=Depends(get_db)):
+def update_post(id: int, post: schemas.PostCreate, db: Session=Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
 
     updated_post=db.query(models.Post).filter(models.Post.id == id)
 
